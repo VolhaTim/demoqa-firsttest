941c239470fa05c0cb1f5834f202a2b34149cdfc
VolhaTim <volha.tim.87@gmail.com>
2023-07-07 00:09:48 +0300

    added steps

--- src/test/java/com/demoqa/tests/PracticeForm.java
+++ src/test/java/com/demoqa/tests/PracticeForm.java
@@ -23,7 +23,7 @@ public class PracticeForm {
 }
     @Test
     @Tag("remote")
-    @DisplayName("Successfull registration test")
+    @DisplayName("Successful registration")
     void successPracticeFormTest(){
 
         step ("Open form", () -> {
